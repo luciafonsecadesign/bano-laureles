3d9ffe9eb8888e6bea762a23a42b1348b3e1c3ad
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-09-14 14:50:31 -0500

    fix op2

--- op2/data.js
+++ op2/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-bao-alcoba-1",
-      "name": "Baño alcoba 1",
+      "id": "0-rea-social",
+      "name": "Área social",
       "levels": [
         {
           "tileSize": 256,
@@ -30,17 +30,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.17773001893882068,
-          "pitch": 0.27651177621918777,
-          "rotation": 7.853981633974483,
-          "target": "1-bao-alcoba-11"
+          "yaw": 0.40527392557014785,
+          "pitch": 0.22084952853697182,
+          "rotation": 0,
+          "target": "3-bao-social-11"
+        },
+        {
+          "yaw": -1.0476104010268337,
+          "pitch": 0.16090788697060177,
+          "rotation": 4.71238898038469,
+          "target": "1-rea-pasillo"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-bao-alcoba-11",
-      "name": "baño alcoba 1.1",
+      "id": "1-rea-pasillo",
+      "name": "Área pasillo",
       "levels": [
         {
           "tileSize": 256,
@@ -68,15 +74,15 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.649209774740882,
-          "pitch": 0.3370982869655723,
-          "rotation": 0,
-          "target": "0-bao-alcoba-1"
+          "yaw": -1.5566389811552028,
+          "pitch": 0.32881048887681885,
+          "rotation": 4.71238898038469,
+          "target": "0-rea-social"
         },
         {
-          "yaw": 1.4506624305698317,
-          "pitch": 0.3648502072212505,
-          "rotation": 7.853981633974483,
+          "yaw": -3.0524370774338117,
+          "pitch": 0.3948417018009174,
+          "rotation": 0,
           "target": "4-alcoba"
         }
       ],
@@ -115,7 +121,7 @@ var APP_DATA = {
           "yaw": -0.35120598206334463,
           "pitch": 0.1652807940613581,
           "rotation": 1.5707963267948966,
-          "target": "6-rea-social"
+          "target": "0-rea-social"
         }
       ],
       "infoHotspots": []
@@ -191,20 +197,20 @@ var APP_DATA = {
           "yaw": 0.07361240301056071,
           "pitch": 0.17625211572982558,
           "rotation": 4.71238898038469,
-          "target": "1-bao-alcoba-11"
+          "target": "6-bao-alcoba-11"
         },
         {
           "yaw": 0.8879520117787436,
           "pitch": 0.2409200240366971,
           "rotation": 1.5707963267948966,
-          "target": "5-rea-pasillo"
+          "target": "1-rea-pasillo"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-rea-pasillo",
-      "name": "Área pasillo",
+      "id": "5-bao-alcoba-1",
+      "name": "Baño alcoba 1",
       "levels": [
         {
           "tileSize": 256,
@@ -232,66 +238,60 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -1.5566389811552028,
-          "pitch": 0.32881048887681885,
-          "rotation": 4.71238898038469,
-          "target": "6-rea-social"
+          "yaw": 0.17773001893882068,
+          "pitch": 0.27651177621918777,
+          "rotation": 7.853981633974483,
+          "target": "6-bao-alcoba-11"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-bao-alcoba-11",
+      "name": "baño alcoba 1.1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": -3.0524370774338117,
-          "pitch": 0.3948417018009174,
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.649209774740882,
+          "pitch": 0.3370982869655723,
           "rotation": 0,
+          "target": "5-bao-alcoba-1"
+        },
+        {
+          "yaw": 1.4506624305698317,
+          "pitch": 0.3648502072212505,
+          "rotation": 7.853981633974483,
           "target": "4-alcoba"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-rea-social",
-      "name": "Área social",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.40527392557014785,
-          "pitch": 0.22084952853697182,
-          "rotation": 0,
-          "target": "3-bao-social-11"
-        },
-        {
-          "yaw": -1.0476104010268337,
-          "pitch": 0.16090788697060177,
-          "rotation": 4.71238898038469,
-          "target": "5-rea-pasillo"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "BAÑO OP 2",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
